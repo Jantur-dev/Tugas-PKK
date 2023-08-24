85eb968946ccf8eb4ca5a649151c52195bad685c
Jantur-dev <jantur29@gmail.com>
2023-08-24 21:33:38 +0700

    manggil component view category sisi admin

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,20 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            @if (session('message'))
-                <div class="alert alert-success">{{ session('message') }}</div>
-            @endif
-
-            <div class="card">
-                <div class="card-header">
-                    <h4>Category
-                        <a href="/admin/category/create" class="btn btn-primary float-end">Add Category</a>
-                    </h4>
-                </div>
-                <div class="card-body"></div>
-            </div>
-        </div>
+    <div>
+        <livewire:admin.category.index />
     </div>
 @endsection
