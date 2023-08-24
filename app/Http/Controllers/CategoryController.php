3a7f40a64a4403551fb878251466ce467841f159
Jantur-dev <jantur29@gmail.com>
2023-08-24 21:29:57 +0700

    manggil Facades/File

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use App\Http\Requests\CategoryFormRequest;
 
 class CategoryController extends Controller
@@ -73,15 +74,49 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.category.edit', ['category' => $category]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(CategoryFormRequest $request, $id)
     {
-        //
+        $category = Category::findOrfail($id);
+
+        $validatedData = $request->validated();
+
+        $category->name = $validatedData['name'];
+        $category->slug = Str::slug($validatedData['slug']);
+        $category->description = $validatedData['description'];
+
+        // cek apakah name image ada filenya? jika ada maka...
+        if ($request->hasFile('image')) {
+            // cek apakah file tersebut ada/tidak
+            $path = 'uploads/category' . $category->image;
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+            // ambil file seklaigus ekstensinya di request name image
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            // buat nama dari file imagenya
+            $filename = time() . '.' . $ext;
+
+            // pindahkan ke folder uploads/category
+            $file->move('uploads/category/', $filename);
+            $category->image = $filename;
+        }
+
+        $category->meta_title = $validatedData['meta_title'];
+        $category->meta_description = $validatedData['meta_description'];
+        $category->meta_keyword = $validatedData['meta_keyword'];
+
+        $category->status = $request->status == true ? 1 : 0;
+
+        $category->update();
+
+        return redirect('/admin/category')->with('message', 'Category Updated Successfully');
     }
 
     /**
