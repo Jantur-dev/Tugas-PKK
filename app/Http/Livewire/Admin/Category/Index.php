310fd9b54654e8e08b7a4e6b7643edc88880fbc2
Jantur-dev <jantur29@gmail.com>
2023-08-24 21:31:16 +0700

    logic delete data

---
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Category;
+
+use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Facades\File;
+
+class Index extends Component
+{
+    public $category_id;
+
+    public function deleteCategory($category_id)
+    {
+        $this->category_id = $category_id;
+    }
+
+    public function destroyCategory()
+    {
+        $category = Category::find($this->category_id);
+        $path = 'uploads/category/' . $category->image;
+
+        if (File::exists($path)) {
+            File::delete($path);
+        }
+        $category->delete();
+        session()->flash('message', 'Category Deleted');
+    }
+
+    public function render()
+    {
+        $categoris = Category::orderBy('id', 'DESC')->paginate(10);
+        return view('livewire.admin.category.index', ['categories' => $categoris]);
+    }
+}
