52fbabcbf1bda0d8ca034fa4270f27d52dd92711
Jantur-dev <jantur29@gmail.com>
2023-08-24 21:35:04 +0700

    nambah kolom sidebar category

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -1,9 +1,22 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="/admin/dashboard">
                 <i class="mdi mdi-home menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/category">
+                {{-- <i class="mdi mdi-home menu-icon"></i> --}}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-window-sidebar" viewBox="0 0 16 16" style="margin-left: 1px;">
+                    <path
+                        d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                    <path
+                        d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v2H1V3a1 1 0 0 1 1-1h12zM1 13V6h4v8H2a1 1 0 0 1-1-1zm5 1V6h9v7a1 1 0 0 1-1 1H6z" />
+                </svg>
+                <span class="menu-title" style="margin-left: 18px;">Category</span>
             </a>
         </li>
         <li class="nav-item">
