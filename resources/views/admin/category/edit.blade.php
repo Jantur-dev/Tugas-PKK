16f55d101750961d5150f417089cead1f0bb2b76
Jantur-dev <jantur29@gmail.com>
2023-08-24 21:32:09 +0700

    halaman edit data

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Edit Category
+                        <a href="/admin/category/" class="btn btn-primary text-white float-end">Back</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <form action="/admin/category/{{ $category->id }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" class="form-control" placeholder="name"
+                                    value="{{ $category->name }}">
+                                @error('name')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="name">Slug</label>
+                                <input type="text" name="slug" class="form-control" placeholder="slug"
+                                    value="{{ $category->slug }}">
+                                @error('slug')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="description">Description</label>
+                                <textarea name="description" id="descripton" rows="3" class="form-control">{{ $category->description }}</textarea>
+                                @error('description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="image">Image</label>
+                                <input type="file" name="image" class="form-control">
+                                <img src="{{ asset('/uploads/category/' . $category->image) }}" alt="image"
+                                    width="60px">
+                                @error('image')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="status">Status</label> <br>
+                                <input type="checkbox" name="status" {{ $category->status == 1 ? 'checked' : '' }}>
+                            </div>
+
+                            <div class="col-md-12">
+                                <h4>SEO Tags</h4>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="meta_title">Meta Title</label>
+                                <input type="text" name="meta_title" class="form-control"
+                                    value="{{ $category->meta_title }}">
+                                @error('meta_title')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="meta_keyword">Meta Keyword</label>
+                                <textarea name="meta_keyword" id="meta_keyword" rows="3" class="form-control">{{ $category->meta_description }}</textarea>
+                                @error('meta_keyword')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="meta_description">Meta description</label>
+                                <textarea name="meta_description" id="meta_description" rows="3" class="form-control">{{ $category->meta_description }}</textarea>
+                                @error('meta_description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-12 mb-3">
+                                <button type="submit" class="btn btn-primary text-white float-end">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
